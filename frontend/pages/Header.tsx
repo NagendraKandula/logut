8011be554227be2c13eb28da7742dccaf1a53878
NagendraKandula <nagendra14902@gmail.com>
2025-09-17 00:56:03 +0530

    Updated Home page

--- frontend/pages/Header.tsx
+++ frontend/pages/Header.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from "../../styles/Header.module.css";
+import styles from "../styles/Header.module.css";
 
 
 export default function Header() {
@@ -8,18 +8,23 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
+      {/* This is the logo on the left */}
       <div className={styles.logo}>☐ LOGO</div>
+
+      {/* These are the navigation links in the middle */}
       <nav className={styles.nav}>
         <a href="#">Features</a>
         <a href="#">Channels</a>
         <a href="#">Blog</a>
         <a href="#">FAQs</a>
       </nav>
+
+      {/* These are the "Log In" and "Get Started" buttons on the right */}
       <div className={styles.actions}>
          <Link href="/login" className={styles.loginButton}>
           Log In
         </Link>
-       <Link href="/signup" className={styles.getStartedButton}>
+       <Link href="/register" className={styles.getStartedButton}>
        Get Started
        </Link>
       </div>
